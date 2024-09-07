94189b5989c11e13ba3ff89a29160cae0db9c821
aravind <aravindsanalkumar@gmail.com>
2024-09-07 10:50:13 +0530

    done with async js

---
+++ script.js
@@ -0,0 +1,37 @@
+const waitingForSoup = () => console.log('soup');
+
+console.log("You start the convo with your friend");
+
+setTimeout(waitingForSoup, 2000);
+
+console.log("still speaking");
+
+
+
+const promise1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        isReady = false;
+        isReady ? resolve('soup is ready resolved') : reject('reject no soup today');
+    },2000)
+});
+
+
+console.log(
+    promise1
+        .then(value => console.log(value))
+        .catch(value => console.log(value))
+);
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+.then(data => console.log(data));
+
+const getDog =async () => {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
+getDog();
